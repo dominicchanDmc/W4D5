2c36af35bc9921b5c4f2e0c82a1abc7a57129ac7
Manuel P <m.perezasensio@gmail.com>
2023-06-16 14:30:34 -0700

    two_sum completed

--- lib/tdd.rb
+++ lib/tdd.rb
@@ -1,3 +1,26 @@
-class Tdd
+class Array
+    
+    def my_uniq
+        return self if self.empty?
+        new_arr = []
+        count = Hash.new(0)
+        self.each do |ele|
+            if count[ele] == 0
+                count[ele] +=1
+                new_arr << ele
+            end
+        end
+        new_arr
+    end
+
+    def two_sum
+        new_arr = []
+        (0...self.length - 1).each do |i|
+            (i + 1...self.length).each do |j|
+                new_arr << [i, j] if self[i] + self[j] == 0
+            end
+        end
+        new_arr
+    end
     
 end
--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -6,19 +6,23 @@ describe Array do
     let(:arr) { [1,2,3,1] }
     let(:arr_1) { [2,2,3,3,1] }
     let(:empty_arr) { [] }
+    let(:arr_2) { [1,2,3,-1] }
+    let(:arr_3) { [2,-2,3,-3,1] }
+    # let(:arr_4) { [3,-3,2,-2,1] }
     
     describe "#my_uniq" do  
       it "should return empty array if input a empty array" do 
         expect(empty_arr.my_uniq).to eq([])
       end
 
-      it "should return a array" do 
-        expect(arr.my_uniq).to be(Array)
+      it "should return an array" do 
+        expect(arr.my_uniq.class).to be(Array)
       end
 
-      it "should return the original array" do
-        return_val = arr.my_each { |el| el }
-        expect(return_val).to eq(arr)
+      it "should not modify the original array" do
+        arr_dup = arr.dup
+         arr.my_uniq 
+        expect(arr).to eq(arr_dup)
       end
      
       it "should return a new array" do 
@@ -30,10 +34,36 @@ describe Array do
         expect(arr_1.my_uniq).to eq([2,3,1])
       end
     end
+
+    # let(:pair_array){[-1, 0, 2, -2, 1]}
+
+    describe "#two_sum" do  
+      it 'should return an empty array when no pairs sum 0' do 
+         expect(empty_arr.two_sum).to eq([])
+      end
+      it "should not modify the original array" do
+        arr_dup = arr.dup
+         arr.two_sum 
+        expect(arr).to eq(arr_dup)
+      end
+
+      it "should return correct result" do 
+        expect(arr_2.two_sum).to eq([[0,3]])
+        expect(arr_3.two_sum).to eq([[0,1], [2,3]])
+      end
+    end
+
+
 end
 
         # empty?                           checked
         # check return is array?           checked
         # check not mofiy th eorg array    checked
-        # check is it a new array?         checked
         # correct result                   checked
+
+
+
+        #return empty when no pairs       checked
+        #should not modify the original array
+        #needs to have the correct order
+        
